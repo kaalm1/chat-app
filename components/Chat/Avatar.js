a683523933f0928d0e1fef2eaae719a607675749
Joe <kaalm1@gmail.com>
2019-09-02 18:14:01 -0700

    adding firebase for backend

--- components/Chat/Avatar.js
+++ components/Chat/Avatar.js
@@ -7,19 +7,18 @@ import NoImage from './NoImage'
 export default class Avatar extends React.Component{
 
   render(){
-
-    let url = this.props.url !== global.NO_PICTURE_URL ? {uri: this.props.url} : this.props.match.gender === 'male' ? defaultImageMale : defaultImageFemale
-    if (this.props.thisUser){return null}
-
     return(
       !this.props.thisUser &&
       !isSameUser(this.props.currentMessage, this.props.previousMessage) ?
+      this.props.url ?
       <Image
         style={{height:40, width:40, borderRadius: 20, margin:10, alignSelf:'flex-end'}}
-        source={url}
+        source={{uri: this.props.url}}
       />
       :
       <NoImage style={{height:40, width:40, borderRadius:20, margin:10, alignSelf:'flex-end'}}/>
+      :
+      <View style={{height:40, width:40, margin:10, alignSelf:'flex-end'}}/>
     )
   }
 }
